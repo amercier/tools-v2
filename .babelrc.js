bd0ebc2ad57d90debf581e9a9ad1a6e97fbeff23
Alex Mercier <pro.alexandre.mercier@gmail.com>
2020-08-24 23:57:31 +0200

    Added Storybook

---
+++ .babelrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+  plugins: [
+    // Next.js doesn't require importing React on each `.jsx` file. To get the
+    // same behavior on Storybook, we add babel-plugin-react-require.
+    'react-require',
+  ],
+}
