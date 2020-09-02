eb0fc79a6e5ffe3c49fc7bf53c81f78e6c92e952
Alex Mercier <pro.alexandre.mercier@gmail.com>
2020-09-02 14:53:59 +0200

    Add Roboto font

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,7 +1,21 @@
+import { Container } from 'next/app'
+import Head from 'next/head'
+
 import '../styles/globals.css'
 
 export default function App({ Component = null, pageProps = {} }) {
   // Exceptionally allow spreading because it's using Next.js API.
-  // eslint-disable-next-line react/jsx-props-no-spreading
-  return <Component {...pageProps} />
+  return (
+    <Container>
+      <Head>
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+        />
+      </Head>
+
+      {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+      <Component {...pageProps} />
+    </Container>
+  )
 }
