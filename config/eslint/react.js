207fb40daf7597fb997cfdefa763eb3e424205a4
Alex Mercier <pro.alexandre.mercier@gmail.com>
2020-12-30 09:41:21 +0100

    Add eslint-plugin-react-hooks
    # Conflicts:
    #	package.json
    #	yarn.lock

--- config/eslint/react.js
+++ config/eslint/react.js
@@ -1,4 +1,5 @@
 module.exports = {
+  extends: ['plugin:react-hooks/recommended'],
   rules: {
     'react/prop-types': 'off',
     'react/react-in-jsx-scope': 'off',
@@ -12,6 +13,12 @@ module.exports = {
             message: 'Importing React is not needed, please remove.',
           },
         ],
+      },
+    ],
+    'react-hooks/exhaustive-deps': [
+      'error',
+      {
+        additionalHooks: 'use[A-Z].*',
       },
     ],
   },
