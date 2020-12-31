c1b985868d4ffc7d4fc5ab1bdb4a57798c1b2ded
Alex Mercier <pro.alexandre.mercier@gmail.com>
2020-12-31 10:58:35 +0100

    Setup TypeScript

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,1 +1,17 @@
-module.exports = require('./config/eslint')
+module.exports = {
+  root: true,
+
+  extends: ['amercier/next-typescript', 'amercier/addons/storybook'],
+
+  rules: {
+    // TODO Remove once integrated in eslint-config-amercier
+    'react/require-default-props': 'off',
+  },
+
+  overrides: [
+    {
+      files: ['config/**', 'scripts/**', '*Story.*'],
+      extends: ['amercier/addons/dev'],
+    },
+  ],
+}
--- config/eslint/index.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  extends: [
-    require.resolve('./base.js'),
-    require.resolve('./import.js'),
-    require.resolve('./react.js'),
-    require.resolve('./jest.js'),
-  ],
-}
