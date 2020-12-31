8760a4ca0843ecfe6e1c0367343f13dad7532c4a
Alex Mercier <pro.alexandre.mercier@gmail.com>
2020-12-31 08:25:23 +0100

    Add pages title

--- src/layouts/DefaultLayout.jsx
+++ src/layouts/DefaultLayout.jsx
@@ -1,9 +1,14 @@
 import { Container, CssBaseline, Typography } from '@material-ui/core'
+import Head from 'next/head'
 
 export default function DefaultLayout({ title, children }) {
   return (
     <>
       <CssBaseline />
+
+      <Head>
+        <title>{title}</title>
+      </Head>
 
       <Container maxWidth="md">
         <div className="root">
