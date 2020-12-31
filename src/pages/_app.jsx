832e7b5fe60a3c435fa2ea8e4f9821cb4b178696
Alex Mercier <pro.alexandre.mercier@gmail.com>
2020-12-31 08:21:56 +0100

    Add viewport meta

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -5,6 +5,7 @@ export default function App({ Component = null, pageProps = {} }) {
   return (
     <>
       <Head>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <link
           rel="stylesheet"
           href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
