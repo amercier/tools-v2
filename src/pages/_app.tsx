429f599cb14aed0aa37bcbbae8d722b2e04d0d6a
Alex Mercier <pro.alexandre.mercier@gmail.com>
2021-01-01 16:25:18 +0100

    Generate CSS server-side, fix FOUC

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,19 +1,10 @@
 import type { AppProps } from 'next/app'
-import Head from 'next/head'
 import { FC } from 'react'
 
 const App: FC<AppProps> = ({ Component, pageProps = {} }) => {
   // Exceptionally allow spreading because it's using Next.js API.
   return (
     <>
-      <Head>
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        <link
-          rel="stylesheet"
-          href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Mono:wght&display=swap"
-        />
-      </Head>
-
       {/* eslint-disable-next-line react/jsx-props-no-spreading */}
       <Component {...pageProps} />
 
---
+++ src/pages/_document.tsx
@@ -0,0 +1,75 @@
+import { ServerStyleSheets } from '@material-ui/core/styles'
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+import { Children } from 'react'
+
+// Fix FOUC
+// Read more: https://github.com/mui-org/material-ui/blob/master/examples/nextjs/pages/_document.js
+export default class AppDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <meta
+            name="viewport"
+            content="initial-scale=1.0, width=device-width"
+          />
+          <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Mono:wght&display=swap"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+// `getInitialProps` belongs to `_document` (instead of `_app`),
+// it's compatible with server-side generation (SSG).
+AppDocument.getInitialProps = async (ctx) => {
+  // Resolution order
+  //
+  // On the server:
+  // 1. app.getInitialProps
+  // 2. page.getInitialProps
+  // 3. document.getInitialProps
+  // 4. app.render
+  // 5. page.render
+  // 6. document.render
+  //
+  // On the server with error:
+  // 1. document.getInitialProps
+  // 2. app.render
+  // 3. page.render
+  // 4. document.render
+  //
+  // On the client
+  // 1. app.getInitialProps
+  // 2. page.getInitialProps
+  // 3. app.render
+  // 4. page.render
+
+  // Render app and page and get the context of the page with collected side effects.
+  const sheets = new ServerStyleSheets()
+  const originalRenderPage = ctx.renderPage
+
+  ctx.renderPage = () =>
+    originalRenderPage({
+      // eslint-disable-next-line react/jsx-props-no-spreading
+      enhanceApp: (App) => (props) => sheets.collect(<App {...props} />),
+    })
+
+  const initialProps = await Document.getInitialProps(ctx)
+
+  return {
+    ...initialProps,
+    // Styles fragment is rendered after the app and page rendering finish.
+    styles: [
+      ...Children.toArray(initialProps.styles),
+      sheets.getStyleElement(),
+    ],
+  }
+}
